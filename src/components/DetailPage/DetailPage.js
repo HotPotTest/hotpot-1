90020854f031890614d669b4e36087e58714c43d
omkar randive <38181204+randiveomkar95@users.noreply.github.com>
2023-03-25 11:53:37 +0530

    added
    added

--- src/components/DetailPage/DetailPage.js
+++ src/components/DetailPage/DetailPage.js
@@ -14,16 +14,18 @@ import Tab from '@mui/material/Tab';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
-
+import {GetMovieList} from '../Api/index'
 const DetailPage = (props) => {
   const {id, category} = useParams();
   useEffect(() => { getData() }, [id])
   const [data, setData] = useState({})
   const getData = async () => {
-    const url = `https://api.themoviedb.org/3/${category}/${id}?api_key=3e3f0a46d6f2abc8e557d06b3fc21a77&language=en-US`
-    const a = await axios.get(url)
-    setData(a.data)
+    GetMovieList().then((res)=>{
+    let DataID = res?.data?.movies.filter((v)=>v._id == id)
+    setData(DataID[0])
+  })
   }
+  console.log(data)
 
   let baseImgUrl = 'http://localhost:3000/'
 //   let baseImgUrl = 'https://image.tmdb.org/t/p/original'
@@ -36,7 +38,7 @@ const DetailPage = (props) => {
   return (
     <>
     <div key={props.pageId}>
-      <Banner original_title={data.original_title} id={id}  img={`${baseImgUrl}${'banner1.jpg'}`} title={data.original_title || data.name} description={data.overview}></Banner>
+      <Banner original_title={data.movieName} id={id}  img={`${baseImgUrl}${'banner1.jpg'}`} title={data.movieName || data.movieName} description={data.movieInfo   }></Banner>
       {/* <DirectorCasts row_title="Directors & Casts"></DirectorCasts>
       <Trailer row_title="Trailers & Other"></Trailer>
       <div style={{display:'flex', flexDirection: 'row'}}>
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,50 +1,100 @@
 import React,{useState} from 'react'
+import { signInWithEmailAndPassword,createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../../firebase";
 
-const Login = () => {
+const Login = ({handleClose}) => {
   const [open,setOpen] = useState(false)
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+const openSignUp= () => {
+  setOpen(true)
+  setEmail("")
+  setPassword("")
+}
+const openSignIn = () => {
+  setOpen(false)
+  setEmail("")
+  setPassword("")
+}
+  const signIn = (e) => {
+    e.preventDefault();
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        if(userCredential){
+          localStorage.setItem("userCredential",JSON.stringify(userCredential))
+        }
+        console.log(userCredential);
+        handleClose()
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+  const signUp = (e) => {
+    e.preventDefault();
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        console.log(userCredential);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
   return (
     <div className='main'>
-     {!open ? 
+      {!open ? 
+      <form onSubmit={signIn}>
      <>
       <p style={{
           fontSize: "18px",
           wordSpacing: "1.4px"
-        }} className="text">Login or Signup</p>
+        }} className="text">Login</p>
       
         <div className="input-div">
-          <input type="email" className="input" placeholder="Enter Email Address" required />
+          <input type="email" className="input" placeholder="Enter Email Address" required
+          onChange={(e)=>setEmail(e.target.value)}
+          />
         </div>
         <div className="input-div">
-          <input type="password" className="input" placeholder="Enter Password" required />
+          <input type="password" className="input" placeholder="Enter Password" required
+            onChange={(e)=>setPassword(e.target.value)}
+
+          />
         </div>
         <div className='button-wrapper'>
-        <button onClick={() => {
-          setOpen(true)
-            // setOtpPopup(true);
-            // setButtonPopup(false)
-          }} className="otp-btn">Submit</button>
+        <button type="submit"
+           className="otp-btn">Submit</button>
           </div>
-          </>:
-          <>
-          <p style={{
-          fontSize: "18px",
-          wordSpacing: "1.4px"
-        }} className="text">Enter otp </p>
-      
-        <div className="input-div">
-      <input type="text" className="input" placeholder="Enter your otp" required />
-          
-        </div>
-        <div className='button-wrapper'>
-        <button onClick={() => {
-            // setOtpPopup(true);
-            // setButtonPopup(false)
-          }} className="otp-btn">Submit</button>
-          </div>
-          
+ <p onClick={openSignUp}>Click here to sign Up</p>      
           </>
           
-          }
+          </form> :
+          <form onSubmit={signUp}>
+          <>
+           <p style={{
+               fontSize: "18px",
+               wordSpacing: "1.4px"
+             }} className="text">Signup</p>
+           
+             <div className="input-div">
+               <input type="email" className="input" placeholder="Enter Email Address" required
+               onChange={(e)=>setEmail(e.target.value)}
+               />
+             </div>
+             <div className="input-div">
+               <input type="password" className="input" placeholder="Enter Password" required
+                 onChange={(e)=>setPassword(e.target.value)}
+     
+               />
+             </div>
+             <div className='button-wrapper'>
+             <button type="submit"
+                className="otp-btn">Submit</button>
+               </div>
+      <p onClick={openSignIn}>Click here to Login</p>      
+               </>
+               
+               </form>}
 
    
     </div>
